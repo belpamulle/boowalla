c54500cbbc49f2532c50d53cf55fbc1ab76849cd
gayan b <gayan.belpamulle@medibank.com.au>
2024-04-03 11:39:04 +1100

    init project
    just basic setup generated from spring start

---
+++ spring-config-deep/src/main/java/com/belpamulle/spring/SpringConfigDeepApplication.java
@@ -0,0 +1,13 @@
+package com.belpamulle.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigDeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigDeepApplication.class, args);
+	}
+
+}
---
+++ spring-config-deep/src/test/java/com/belpamulle/spring/SpringConfigDeepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belpamulle.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigDeepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
